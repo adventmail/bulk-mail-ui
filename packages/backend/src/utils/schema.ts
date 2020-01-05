d7825758ae72d059870fcb9409f412226c2cff01
Kumar Abhirup <kumar@iqubex.com>
2020-01-05 17:46:53 +0530

    📦 NEW: Write sendMail Mutation and schema

--- packages/backend/src/utils/schema.ts
+++ packages/backend/src/utils/schema.ts
@@ -1,9 +1,58 @@
 const typeDefs = `
-  type Mutation {
-    exampleMutation: String
-    sendMails: String
+  # Bmc Configuration Settings
+  input BmcCredential {
+    email: String!
+    password: String!
+    host: String!
+    port: Int!
+    secureConnection: Boolean!
+    proxy: String
   }
 
+  input BmcAttachment {
+    filename: String!
+    path: String!
+  }
+
+  input BmcMailSettings {
+    subject: String!
+    from: String!
+    to: String! # This should be stringified version of json object that contains the CsvData.
+    theme: String! # This should be stringified version of an HTML document.
+    attachments: [BmcAttachment]
+  }
+
+  input BmcConfigurations {
+    mailInterval: String
+    verbose: Boolean
+  }
+
+  input BmcNonUserData {
+    sentTo: [String]!
+  }
+
+  input BmcMasterConfiguration {
+    credentials: BmcCredential!
+    mail: BmcMailSettings!
+    configuration: BmcConfigurations
+    nonUserData: BmcNonUserData
+  }
+
+  # Returned data after Mails Sent
+  type MailSentResponse {
+    message: String!
+  }
+
+  # Mutations
+  type Mutation {
+    exampleMutation: String
+    sendMails(
+      configuration: BmcMasterConfiguration
+      shouldRestart: Boolean = false
+    ): MailSentResponse!
+  }
+
+  # Queries
   type Query {
     exampleQuery: String
   }
